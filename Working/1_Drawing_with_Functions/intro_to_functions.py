36b0dedb9666baa1387591daeec210cfc66a634c
josh <joshua.chuang21@ycdsbk12.ca>
2019-03-05 10:15:10 -0500

    new tree

--- Working/1_Drawing_with_Functions/intro_to_functions.py
+++ Working/1_Drawing_with_Functions/intro_to_functions.py
@@ -16,7 +16,18 @@ def draw_rollinghills():
     arcade.draw_circle_filled(600, 0, 300, arcade.color.GREEN)
 
 def draw_tree():
-    
+    arcade.draw_rectangle_filled(600, 200, 100, 300, arcade.color.BROWN)
+    arcade.draw_circle_filled(525, 400, 75, arcade.color.DARK_PASTEL_GREEN)
+    arcade.draw_circle_filled(550, 425, 75, arcade.color.DARK_PASTEL_GREEN)
+    arcade.draw_circle_filled(550, 450, 75, arcade.color.DARK_PASTEL_GREEN)
+    arcade.draw_circle_filled(600, 500, 75, arcade.color.DARK_PASTEL_GREEN)
+    arcade.draw_circle_filled(650, 450, 75, arcade.color.DARK_PASTEL_GREEN)
+    arcade.draw_circle_filled(650, 425, 75, arcade.color.DARK_PASTEL_GREEN)
+    arcade.draw_circle_filled(675, 400, 75, arcade.color.DARK_PASTEL_GREEN)
+    arcade.draw_circle_filled(600, 375, 75, arcade.color.DARK_PASTEL_GREEN)
+
+
+
 def main():
     arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Drawing with Functions")
     arcade.set_background_color(arcade.color.LIGHT_BLUE)
@@ -25,6 +36,7 @@ def main():
    # call your draw functions
     draw_cloud()
     draw_rollinghills()
+    draw_tree()
     # Finish and run
     arcade.finish_render()
     arcade.run()
