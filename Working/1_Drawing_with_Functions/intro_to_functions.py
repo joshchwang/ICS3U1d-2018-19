6055254f9afb932c4a84b3a75b6d6dbdbe1112b3
josh <joshua.chuang21@ycdsbk12.ca>
2019-03-06 11:02:50 -0500

    new tree

--- Working/1_Drawing_with_Functions/intro_to_functions.py
+++ Working/1_Drawing_with_Functions/intro_to_functions.py
@@ -4,27 +4,29 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
 # define your draw functions
-def draw_cloud():
-    arcade.draw_circle_filled(100, 450, 50, arcade.color.WHITE)
-    arcade.draw_circle_filled(150, 500, 50, arcade.color.WHITE)
-    arcade.draw_circle_filled(175, 450, 50, arcade.color.WHITE)
-    arcade.draw_circle_filled(200, 500, 50, arcade.color.WHITE)
-    arcade.draw_circle_filled(250, 450, 50, arcade.color.WHITE)
 
-def draw_rollinghills():
-    arcade.draw_circle_filled(200, 0, 300, arcade.color.GREEN)
-    arcade.draw_circle_filled(600, 0, 300, arcade.color.GREEN)
+def draw_cloud(x, y, size):
+    arcade.draw_circle_filled(x, y, size, arcade.color.WHITE)
+    arcade.draw_circle_filled(x + 50, y + 50, size, arcade.color.WHITE)
+    arcade.draw_circle_filled(x + 75, y, size, arcade.color.WHITE)
+    arcade.draw_circle_filled(x + 100, y + 50, size, arcade.color.WHITE)
+    arcade.draw_circle_filled(x + 150, y, size, arcade.color.WHITE)
 
-def draw_tree():
-    arcade.draw_rectangle_filled(600, 200, 100, 300, arcade.color.BROWN)
-    arcade.draw_circle_filled(525, 400, 75, arcade.color.DARK_PASTEL_GREEN)
-    arcade.draw_circle_filled(550, 425, 75, arcade.color.DARK_PASTEL_GREEN)
-    arcade.draw_circle_filled(550, 450, 75, arcade.color.DARK_PASTEL_GREEN)
-    arcade.draw_circle_filled(600, 500, 75, arcade.color.DARK_PASTEL_GREEN)
-    arcade.draw_circle_filled(650, 450, 75, arcade.color.DARK_PASTEL_GREEN)
-    arcade.draw_circle_filled(650, 425, 75, arcade.color.DARK_PASTEL_GREEN)
-    arcade.draw_circle_filled(675, 400, 75, arcade.color.DARK_PASTEL_GREEN)
-    arcade.draw_circle_filled(600, 375, 75, arcade.color.DARK_PASTEL_GREEN)
+def draw_rollinghills(w, z, sizes):
+    arcade.draw_circle_filled(w, z, sizes, arcade.color.GREEN)
+    arcade.draw_circle_filled(w + 400, z, sizes, arcade.color.GREEN)
+
+def draw_tree(trunkx, trunky, width, height, circlex, circley, circlesize):
+    arcade.draw_rectangle_filled(trunkx, trunky, width, height, arcade.color.BROWN)
+    arcade.draw_circle_filled(circlex, circley, circlesize, arcade.color.DARK_PASTEL_GREEN)
+    arcade.draw_circle_filled(circlex + 25, circley + 25, circlesize, arcade.color.DARK_PASTEL_GREEN)
+    arcade.draw_circle_filled(circlex + 25, circley + 50, circlesize, arcade.color.DARK_PASTEL_GREEN)
+    arcade.draw_circle_filled(circlex + 75, circley + 100, circlesize, arcade.color.DARK_PASTEL_GREEN)
+    arcade.draw_circle_filled(circlex + 125, circley + 50, circlesize, arcade.color.DARK_PASTEL_GREEN)
+    arcade.draw_circle_filled(circlex + 125, circley + 25, circlesize, arcade.color.DARK_PASTEL_GREEN)
+    arcade.draw_circle_filled(circlex + 150, circley, circlesize, arcade.color.DARK_PASTEL_GREEN)
+    arcade.draw_circle_filled(circlex + 75, circley - 25, circlesize, arcade.color.DARK_PASTEL_GREEN)
+
 
 
 
@@ -34,9 +36,9 @@ def main():
     arcade.start_render()
 
    # call your draw functions
-    draw_cloud()
-    draw_rollinghills()
-    draw_tree()
+    draw_cloud(100, 450, 50)
+    draw_rollinghills(200, 0, 300)
+    draw_tree(600, 200, 100, 300, 525, 400, 75)
     # Finish and run
     arcade.finish_render()
     arcade.run()
