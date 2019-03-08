dc646e52a4cd136c9d503a053dd5fb960435f32c
josh <joshua.chuang21@ycdsbk12.ca>
2019-03-08 10:59:59 -0500

    completed the return statements joyyyyy

--- Working/volumes.py
+++ Working/volumes.py
@@ -4,15 +4,19 @@ radius = int(input("Please input the radius: "))
 height = int(input("Please input the height: "))
 def cylinder(r, h):
     cyl = math.pi*r**2*h
+    print("The volume of a cylinder is: ")
     print(round(cyl, 1))
 
 def cone(r, h):
     con = 1/3*math.pi*r**2*h
+    print("The volume of a cone is: ")
     print(round(con, 1))
+
 
 def sphere(r):
     sph = 4/3*math.pi*r**3
-    print(round(sph, 1))
+    print("The volume of a sphere is: ")
+    print(round(sph, 5))
 
 # Call the main function to get the program started.
 cylinder(radius, height)
